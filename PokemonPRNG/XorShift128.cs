23b79d66d902524d219fa282c3ee12746cfe32ec
yatsuna827 <yatsuna_827@yahoo.co.jp>
2021-12-14 02:58:13 +0900

    xorshift.backの修正

--- PokemonPRNG/XorShift128.cs
+++ PokemonPRNG/XorShift128.cs
@@ -23,14 +23,11 @@ namespace PokemonPRNG.XorShift128
 
         public static (uint s0, uint s1, uint s2, uint s3) Back(ref this (uint s0, uint s1, uint s2, uint s3) state)
         {
-            var t11 = state.s2 ^ state.s2 >> 19;
-            var t12 = t11 ^ (t11 >> 8) ^ (t11 >> 16) ^ (t11 >> 24);
-            var t13 = t12 ^ (t12 << 11) ^ (t12 << 22);
+            var t = state.s3 ^ state.s2 ^ (state.s2 >> 19);
+            t = t ^ (t >> 8) ^ (t >> 16) ^ (t >> 24);
+            t = t ^ (t << 11) ^ (t << 22);
 
-            var t21 = state.s3 ^ (state.s3 >> 8) ^ (state.s3 >> 16) ^ (state.s3 >> 24);
-            var t22 = t21 ^ (t21 << 11) ^ (t21 << 22);
-
-            return state = (t13 ^ t22, state.s1, state.s2, state.s3);
+            return state = (t, state.s0, state.s1, state.s2);
         }
     }
 }
