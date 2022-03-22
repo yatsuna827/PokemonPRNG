ba56c45f9f6b1a9f2a1992288ac8d7cee8bd7540
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-03-22 23:17:39 +0900

    update xorshift

--- PokemonPRNG/XorShift128.cs
+++ PokemonPRNG/XorShift128.cs
@@ -12,6 +12,36 @@ namespace PokemonPRNG.XorShift128
 
             return t2;
         }
+        public static uint GetRand(ref this (uint s0, uint s1, uint s2, uint s3) state, uint n)
+        {
+            var t1 = state.s0 ^ (state.s0 << 11);
+            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+
+            state = (state.s1, state.s2, state.s3, t2);
+
+            return t2 % n;
+        }
+
+        public static float GetRand_f(ref this (uint s0, uint s1, uint s2, uint s3) state)
+        {
+            var t1 = state.s0 ^ (state.s0 << 11);
+            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+
+            state = (state.s1, state.s2, state.s3, t2);
+
+            return (t2 & 0x7F_FFFF) / 8388607.0f;
+        }
+        public static float GetRand_f(ref this (uint s0, uint s1, uint s2, uint s3) state, float min, float max)
+        {
+            var t1 = state.s0 ^ (state.s0 << 11);
+            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+
+            state = (state.s1, state.s2, state.s3, t2);
+
+            var r = (t2 & 0x7F_FFFF) / 8388607.0f;
+
+            return r * min + (1 - r) * max;
+        }
 
         public static (uint s0, uint s1, uint s2, uint s3) Advance(ref this (uint s0, uint s1, uint s2, uint s3) state)
         {
@@ -19,6 +49,13 @@ namespace PokemonPRNG.XorShift128
             var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
 
             return state = (state.s1, state.s2, state.s3, t2);
+        }
+        public static (uint s0, uint s1, uint s2, uint s3) Advance(ref this (uint s0, uint s1, uint s2, uint s3) state, uint n)
+        {
+            for (int i = 0; i < n; i++)
+                state.Advance();
+
+            return state;
         }
 
         public static (uint s0, uint s1, uint s2, uint s3) Back(ref this (uint s0, uint s1, uint s2, uint s3) state)
@@ -28,6 +65,13 @@ namespace PokemonPRNG.XorShift128
             t = t ^ (t << 11) ^ (t << 22);
 
             return state = (t, state.s0, state.s1, state.s2);
+        }
+        public static (uint s0, uint s1, uint s2, uint s3) Back(ref this (uint s0, uint s1, uint s2, uint s3) state, uint n)
+        {
+            for (int i = 0; i < n; i++)
+                state.Back();
+
+            return state;
         }
     }
 }
