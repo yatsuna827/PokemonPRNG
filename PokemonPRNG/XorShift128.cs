1447e57139903338fa85d1f150d96b76caa3bbd8
yatsuna827 <yatsuna_827@yahoo.co.jp>
2022-04-07 23:12:15 +0900

    非破壊的メソッドを追加

--- PokemonPRNG/XorShift128.cs
+++ PokemonPRNG/XorShift128.cs
@@ -43,6 +43,20 @@ namespace PokemonPRNG.XorShift128
             return r * min + (1 - r) * max;
         }
 
+        public static (uint s0, uint s1, uint s2, uint s3) Next(this (uint s0, uint s1, uint s2, uint s3) state)
+        {
+            var t1 = state.s0 ^ (state.s0 << 11);
+            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+
+            return (state.s1, state.s2, state.s3, t2);
+        }
+        public static (uint s0, uint s1, uint s2, uint s3) Next(this (uint s0, uint s1, uint s2, uint s3) state, uint n)
+        {
+            for (int i = 0; i < n; i++)
+                state.Advance();
+
+            return state;
+        }
         public static (uint s0, uint s1, uint s2, uint s3) Advance(ref this (uint s0, uint s1, uint s2, uint s3) state)
         {
             var t1 = state.s0 ^ (state.s0 << 11);
@@ -58,6 +72,21 @@ namespace PokemonPRNG.XorShift128
             return state;
         }
 
+        public static (uint s0, uint s1, uint s2, uint s3) Prev(this (uint s0, uint s1, uint s2, uint s3) state)
+        {
+            var t = state.s3 ^ state.s2 ^ (state.s2 >> 19);
+            t = t ^ (t >> 8) ^ (t >> 16) ^ (t >> 24);
+            t = t ^ (t << 11) ^ (t << 22);
+
+            return (t, state.s0, state.s1, state.s2);
+        }
+        public static (uint s0, uint s1, uint s2, uint s3) Prev(this (uint s0, uint s1, uint s2, uint s3) state, uint n)
+        {
+            for (int i = 0; i < n; i++)
+                state.Back();
+
+            return state;
+        }
         public static (uint s0, uint s1, uint s2, uint s3) Back(ref this (uint s0, uint s1, uint s2, uint s3) state)
         {
             var t = state.s3 ^ state.s2 ^ (state.s2 >> 19);
