4984748e8abe1509c887f32f835add098fde1469
yatsuna827 <yatsuna_827@yahoo.co.jp>
2023-06-26 02:46:31 +0900

    TinyMTの拡張メソッド版を実装

--- PokemonPRNG.TinyMT/TinyMT.cs
+++ PokemonPRNG.TinyMT/TinyMT.cs
@@ -1,8 +1,8 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
-namespace PokemonPRNG
+namespace PokemonPRNG.TinyMT
 {
-    // TODO: ValueTupleのExtensionで書けるバージョンを提供する
     public class TinyMT
     {
         public TinyMT(uint initialSeed)
@@ -74,5 +74,98 @@ namespace PokemonPRNG
         private const uint TMAT = 0x3793fdff;
 
         private readonly uint[] stateVector;
+    }
+
+    public static class TinyMTExt
+    {
+        public static (uint S0, uint S1, uint S2, uint S3) TinyMT(this uint initialSeed)
+        {
+            var stateVector = new uint[4]
+            {
+                initialSeed,
+                0x8f7011ee,
+                0xfc78ff1f,
+                0x3793fdff,
+            };
+
+            for (uint i = 1; i < 8; i++)
+                stateVector[i & 3] ^= i + 0x6C078965u * (stateVector[(i - 1) & 3] ^ (stateVector[(i - 1) & 3] >> 30));
+
+            var state = (stateVector[0], stateVector[1], stateVector[2], stateVector[3]);
+
+            for (int i = 0; i < 8; i++)
+                state.Advance();
+
+            return state;
+        }
+
+        public static uint GetRand(ref this (uint S0, uint S1, uint S2, uint S3) state)
+        {
+            var (x, y) = ((state.S0 & 0x7FFFFFFF) ^ state.S1 ^ state.S2, state.S3);
+            x ^= (x << 1);
+            y ^= (y >> 1) ^ x;
+
+            state = (state.S1, state.S2, x ^ (y << 10), y);
+            if ((y & 1) == 1)
+            {
+                state.S1 ^= 0x8f7011ee;
+                state.S2 ^= 0xfc78ff1f;
+            }
+
+            var (t0, t1) = (state.S3, state.S0 + (state.S2 >> 8));
+
+            t0 ^= t1;
+            if ((t1 & 1) == 1) t0 ^= 0x3793fdff;
+
+            return t0;
+        }
+        public static uint GetRand(ref this (uint S0, uint S1, uint S2, uint S3) state, uint n)
+            => state.GetRand() % n;
+
+        public static (uint S0, uint S1, uint S2, uint S3) Next(this (uint S0, uint S1, uint S2, uint S3) state)
+        {
+            var (x, y) = ((state.S0 & 0x7FFFFFFF) ^ state.S1 ^ state.S2, state.S3);
+            x ^= (x << 1);
+            y ^= (y >> 1) ^ x;
+
+            var (s0, s1, s2, s3) = (state.S1, state.S2, x ^ (y << 10), y);
+            if ((y & 1) == 1)
+            {
+                s1 ^= 0x8f7011ee;
+                s2 ^= 0xfc78ff1f;
+            }
+
+            return (s0, s1, s2, s3);
+        }
+        public static (uint S0, uint S1, uint S2, uint S3) Next(this (uint S0, uint S1, uint S2, uint S3) state, uint n)
+        {
+            for (int i = 0; i < n; i++)
+                state.Advance();
+
+            return state;
+        }
+        public static (uint S0, uint S1, uint S2, uint S3) Advance(ref this (uint S0, uint S1, uint S2, uint S3) state)
+        {
+            var (x, y) = ((state.S0 & 0x7FFFFFFF) ^ state.S1 ^ state.S2, state.S3);
+            x ^= (x << 1);
+            y ^= (y >> 1) ^ x;
+
+            var (s0, s1, s2, s3) = (state.S1, state.S2, x ^ (y << 10), y);
+            if ((y & 1) == 1)
+            {
+                s1 ^= 0x8f7011ee;
+                s2 ^= 0xfc78ff1f;
+            }
+
+            return state = (s0, s1, s2, s3);
+        }
+        public static (uint S0, uint S1, uint S2, uint S3) Advance(ref this (uint S0, uint S1, uint S2, uint S3) state, uint n)
+        {
+            for (int i = 0; i < n; i++)
+                state.Advance();
+
+            return state;
+        }
+
     }
 }
