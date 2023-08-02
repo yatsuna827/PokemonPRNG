3388af7e9f56e498271259979a9aa5a5868a016c
yatsuna827 <yatsuna_827@yahoo.co.jp>
2023-08-03 03:44:55 +0900

    rename

--- PokemonPRNG.XorShift128/XorShift128.cs
+++ PokemonPRNG.XorShift128/XorShift128.cs
@@ -6,80 +6,80 @@ namespace PokemonPRNG.XorShift128
 {
     public static class XorShift128Ext
     {
-        public static uint GetRand(ref this (uint s0, uint s1, uint s2, uint s3) state)
+        public static uint GetRand(ref this (uint S0, uint S1, uint S2, uint S3) state)
         {
-            var t1 = state.s0 ^ (state.s0 << 11);
-            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+            var t1 = state.S0 ^ (state.S0 << 11);
+            var t2 = state.S3 ^ (state.S3 >> 19) ^ t1 ^ (t1 >> 8);
 
-            state = (state.s1, state.s2, state.s3, t2);
+            state = (state.S1, state.S2, state.S3, t2);
 
             return (t2 % 0xFFFFFFFF) + 0x80000000;
         }
-        public static uint GetRand(ref this (uint s0, uint s1, uint s2, uint s3) state, uint n)
+        public static uint GetRand(ref this (uint S0, uint S1, uint S2, uint S3) state, uint n)
         {
-            var t1 = state.s0 ^ (state.s0 << 11);
-            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+            var t1 = state.S0 ^ (state.S0 << 11);
+            var t2 = state.S3 ^ (state.S3 >> 19) ^ t1 ^ (t1 >> 8);
 
-            state = (state.s1, state.s2, state.s3, t2);
+            state = (state.S1, state.S2, state.S3, t2);
 
             return ((t2 % 0xFFFFFFFF) + 0x80000000) % n;
         }
 
-        public static float GetRand_f(ref this (uint s0, uint s1, uint s2, uint s3) state)
+        public static float GetRand_f(ref this (uint S0, uint S1, uint S2, uint S3) state)
         {
-            var t1 = state.s0 ^ (state.s0 << 11);
-            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+            var t1 = state.S0 ^ (state.S0 << 11);
+            var t2 = state.S3 ^ (state.S3 >> 19) ^ t1 ^ (t1 >> 8);
 
-            state = (state.s1, state.s2, state.s3, t2);
+            state = (state.S1, state.S2, state.S3, t2);
 
             return (t2 & 0x7F_FFFF) / 8388607.0f;
         }
-        public static float GetRand_f(ref this (uint s0, uint s1, uint s2, uint s3) state, float min, float max)
+        public static float GetRand_f(ref this (uint S0, uint S1, uint S2, uint S3) state, float min, float max)
         {
-            var t1 = state.s0 ^ (state.s0 << 11);
-            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+            var t1 = state.S0 ^ (state.S0 << 11);
+            var t2 = state.S3 ^ (state.S3 >> 19) ^ t1 ^ (t1 >> 8);
 
-            state = (state.s1, state.s2, state.s3, t2);
+            state = (state.S1, state.S2, state.S3, t2);
 
             var r = (t2 & 0x7F_FFFF) / 8388607.0f;
 
             return r * min + (1 - r) * max;
         }
 
-        public static (uint s0, uint s1, uint s2, uint s3) Next(this (uint s0, uint s1, uint s2, uint s3) state)
+        public static (uint S0, uint S1, uint S2, uint S3) Next(this (uint S0, uint S1, uint S2, uint S3) state)
         {
-            var t1 = state.s0 ^ (state.s0 << 11);
-            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+            var t1 = state.S0 ^ (state.S0 << 11);
+            var t2 = state.S3 ^ (state.S3 >> 19) ^ t1 ^ (t1 >> 8);
 
-            return (state.s1, state.s2, state.s3, t2);
+            return (state.S1, state.S2, state.S3, t2);
         }
-        public static (uint s0, uint s1, uint s2, uint s3) Advance(ref this (uint s0, uint s1, uint s2, uint s3) state)
+        public static (uint S0, uint S1, uint S2, uint S3) Advance(ref this (uint S0, uint S1, uint S2, uint S3) state)
         {
-            var t1 = state.s0 ^ (state.s0 << 11);
-            var t2 = state.s3 ^ (state.s3 >> 19) ^ t1 ^ (t1 >> 8);
+            var t1 = state.S0 ^ (state.S0 << 11);
+            var t2 = state.S3 ^ (state.S3 >> 19) ^ t1 ^ (t1 >> 8);
 
-            return state = (state.s1, state.s2, state.s3, t2);
+            return state = (state.S1, state.S2, state.S3, t2);
         }
 
-        public static (uint s0, uint s1, uint s2, uint s3) Prev(this (uint s0, uint s1, uint s2, uint s3) state)
+        public static (uint S0, uint S1, uint S2, uint S3) Prev(this (uint S0, uint S1, uint S2, uint S3) state)
         {
-            var t = state.s3 ^ state.s2 ^ (state.s2 >> 19);
+            var t = state.S3 ^ state.S2 ^ (state.S2 >> 19);
             t = t ^ (t >> 8) ^ (t >> 16) ^ (t >> 24);
             t = t ^ (t << 11) ^ (t << 22);
 
-            return (t, state.s0, state.s1, state.s2);
+            return (t, state.S0, state.S1, state.S2);
         }
-        public static (uint s0, uint s1, uint s2, uint s3) Back(ref this (uint s0, uint s1, uint s2, uint s3) state)
+        public static (uint S0, uint S1, uint S2, uint S3) Back(ref this (uint S0, uint S1, uint S2, uint S3) state)
         {
-            var t = state.s3 ^ state.s2 ^ (state.s2 >> 19);
+            var t = state.S3 ^ state.S2 ^ (state.S2 >> 19);
             t = t ^ (t >> 8) ^ (t >> 16) ^ (t >> 24);
             t = t ^ (t << 11) ^ (t << 22);
 
-            return state = (t, state.s0, state.s1, state.s2);
+            return state = (t, state.S0, state.S1, state.S2);
         }
 
-        public static string ToU128String(this (uint s0, uint s1, uint s2, uint s3) state) => $"{state.s0:X8}{state.s1:X8}{state.s2:X8}{state.s3:X8}";
-        public static (uint s0, uint s1, uint s2, uint s3) FromU128String(this string hex)
+        public static string ToU128String(this (uint S0, uint S1, uint S2, uint S3) state) => $"{state.S0:X8}{state.S1:X8}{state.S2:X8}{state.S3:X8}";
+        public static (uint S0, uint S1, uint S2, uint S3) FromU128String(this string hex)
         {
             if (hex.Length > 32 || hex.Length == 0) throw new ArgumentException("bad argument");
 
@@ -608,11 +608,11 @@ namespace PokemonPRNG.XorShift128
 
     public interface IGeneratable<out TResult>
     {
-        TResult Generate((uint s0, uint s1, uint s2, uint s3) seed);
+        TResult Generate((uint S0, uint S1, uint S2, uint S3) seed);
     }
     public interface IGeneratable<out TResult, in TArg1>
     {
-        TResult Generate((uint s0, uint s1, uint s2, uint s3) seed, TArg1 arg1);
+        TResult Generate((uint S0, uint S1, uint S2, uint S3) seed, TArg1 arg1);
     }
 
     public static class CommonEnumerator
@@ -620,18 +620,18 @@ namespace PokemonPRNG.XorShift128
         public static IEnumerable<(int index, T element)> WithIndex<T>(this IEnumerable<T> enumerator)
             => enumerator.Select((_, i) => (i, _));
 
-        public static IEnumerable<(uint s0, uint s1, uint s2, uint s3)> EnumerateSeed(this (uint s0, uint s1, uint s2, uint s3) seed)
+        public static IEnumerable<(uint S0, uint S1, uint S2, uint S3)> EnumerateSeed(this (uint S0, uint S1, uint S2, uint S3) seed)
         {
             yield return seed;
             while (true) yield return seed.Advance();
         }
 
         public static IEnumerable<TResult> EnumerateGeneration<TResult>
-            (this IEnumerable<(uint s0, uint s1, uint s2, uint s3)> seedEnumerator, IGeneratable<TResult> igenerator)
+            (this IEnumerable<(uint S0, uint S1, uint S2, uint S3)> seedEnumerator, IGeneratable<TResult> igenerator)
             => seedEnumerator.Select(igenerator.Generate);
 
         public static IEnumerable<TResult> EnumerateGeneration<TResult, TArg1>
-            (this IEnumerable<(uint s0, uint s1, uint s2, uint s3)> seedEnumerator, IGeneratable<TResult, TArg1> igenerator, TArg1 arg1)
+            (this IEnumerable<(uint S0, uint S1, uint S2, uint S3)> seedEnumerator, IGeneratable<TResult, TArg1> igenerator, TArg1 arg1)
             => seedEnumerator.Select(_ => igenerator.Generate(_, arg1));
     }
 }
